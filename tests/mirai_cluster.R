de99b6acd00c08718c752139cf621581cf3d0b00
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-05-04 03:52:12 +0200

    TESTS: Increase timeout for future.tests checks

--- tests/mirai_cluster.R
+++ tests/mirai_cluster.R
@@ -1,5 +1,5 @@
 if (requireNamespace("future.tests")) {
   mirai::daemons(0)
-  future.tests::check("future.mirai::mirai_cluster", timeout = 10.0, exit_value = FALSE)
+  future.tests::check("future.mirai::mirai_cluster", timeout = 30.0, exit_value = FALSE)
   mirai::daemons(0)
 }
