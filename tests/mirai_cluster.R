56c8c34848d939d3aaa714d4e32668ff6d08e7c4
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-04-04 17:08:35 -0700

    TESTS: Clarify what mirai::daemons(0) does [ci skip]

--- tests/mirai_cluster.R
+++ tests/mirai_cluster.R
@@ -1,5 +1,5 @@
 if (requireNamespace("future.tests")) {
-  mirai::daemons(0)
+  mirai::daemons(0)  ## Reset any daemons running
   future.tests::check("future.mirai::mirai_cluster", timeout = 30.0, exit_value = FALSE)
-  mirai::daemons(0)
+  mirai::daemons(0)  ## Reset any daemons running
 }
