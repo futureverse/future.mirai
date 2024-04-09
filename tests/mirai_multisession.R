cda4c18a9b805fbf879634ad5fae522d7a38f2d9
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-04-08 20:12:04 -0700

    Try to avoid NOTEs on MS Windows by not testing

--- tests/mirai_multisession.R
+++ tests/mirai_multisession.R
@@ -1,6 +1,10 @@
 if (requireNamespace("future.tests")) {
   mirai::daemons(0)  ## Reset any daemons running
-  future.tests::check("future.mirai::mirai_multisession", timeout = 30.0, exit_value = FALSE)
+  
+  if (.Platform[["OS.type"]] != "windows") {
+    future.tests::check("future.mirai::mirai_multisession", timeout = 30.0, exit_value = FALSE)
+  }
+  
   mirai::daemons(0)  ## Reset any daemons running
   gc()
 }
