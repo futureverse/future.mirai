0964136fae25cda17ed1aa64199c3d6e40a6362d
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-04-12 12:12:18 -0700

    Examples did not work in R (< 4.0.0), because they used the new lambda notation

--- incl/mirai_multisession.R
+++ incl/mirai_multisession.R
@@ -1,7 +1,7 @@
 plan(mirai_multisession)
 
 # A function that returns a future, note that N uses lexical scoping...
-f <- \() future({4 * sum((runif(N) ^ 2 + runif(N) ^ 2) < 1) / N}, seed = TRUE)
+f <- function() future({4 * sum((runif(N) ^ 2 + runif(N) ^ 2) < 1) / N}, seed = TRUE)
 
 # Run a simple sampling approximation of pi in parallel using  M * N points:
 N <- 1e6  # samples per worker
