e2da9d9c26e4ab7af2a3b5eb1c1a6e514d58e062
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-03-29 14:53:02 -0700

    Troubleshoot detritus files - take 4

--- inst/testme/test-01-daemons.R
+++ inst/testme/test-01-daemons.R
@@ -1,3 +1,5 @@
 mirai::daemons(2)
 print(mirai::status())
 
+## Give daemons a chance to shutdown
+Sys.sleep(10)
--- inst/testme/test-02-daemons-shutdown.R
+++ inst/testme/test-02-daemons-shutdown.R
@@ -3,3 +3,5 @@ print(mirai::status())
 mirai::daemons(0)
 print(mirai::status())
 
+## Give daemons a chance to shutdown
+Sys.sleep(10)
