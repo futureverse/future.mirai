f02feaa84a72c84d251a731927b7a86d80adebf0
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-03-29 14:27:28 -0700

    Troubleshoot detritus files

---
+++ inst/testme/hide/test-demo.R
@@ -0,0 +1,16 @@
+library(future.mirai)
+
+options(future.demo.mandelbrot.nrow = 2L)
+options(future.demo.mandelbrot.resolution = 50L)
+options(future.demo.mandelbrot.delay = FALSE)
+
+message("*** Demos ...")
+
+message("*** Mandelbrot demo of the 'future' package ...")
+
+plan(mirai_multisession, workers = 2)
+demo("mandelbrot", package = "future", ask = FALSE)
+plan(sequential)
+
+message("*** Demos ... DONE")
+
